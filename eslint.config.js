2bdc24cb4986bfe7604f659afabc8f3b5d11dd5b
최유정 <cleame8922@naver.com>
2026-07-12 21:51:48 +0900

    fix: eslint flat config에 @typescript-eslint 플러그인 등록 (#42)

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 const reactCompiler = require('eslint-plugin-react-compiler');
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const tsEslint = require('@typescript-eslint/eslint-plugin');
 
 module.exports = defineConfig([
   expoConfig,
@@ -10,6 +11,9 @@ module.exports = defineConfig([
   eslintPluginPrettierRecommended,
   {
     ignores: ['dist/*', 'plugin/build/*'],
+    plugins: {
+      '@typescript-eslint': tsEslint,
+    },
     rules: {
       '@typescript-eslint/no-explicit-any': 'error',
       'import/no-unresolved': 'off',
